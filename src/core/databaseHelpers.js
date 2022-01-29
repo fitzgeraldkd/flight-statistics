0c723f01bbcee498f24a21d5282c2f4e87f99ca4
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2022-01-29 09:34:35 -0500

    Refactor processStatistics

--- src/core/databaseHelpers.js
+++ src/core/databaseHelpers.js
@@ -8,30 +8,10 @@ export function processStatistics(rawJsonData) {
   for (const record of rawJsonData) {
     airlineFilterOptions.year.add(record.Time.Year);
     airlineFilterOptions.airports.add(record.Airport.Code);
-
     airlineStats[record.Time.Year] ||= {};
     airlineStats[record.Time.Year][record.Airport.Code] ||= {};
     airlineStats[record.Time.Year][record.Airport.Code][record.Time['Month Name']] = record.Statistics
-
-    // if (record.Time.Year in airlineStats) {
-    //   if (record.Airport.Code in airlineStats[record.Time.Year]) {
-    //     airlineStats[record.Time.Year][record.Airport.Code][record.Time['Month Name']] = record.Statistics;
-    //   } else {
-    //     airlineStats[record.Time.Year][record.Airport.Code] = {
-    //       [record.Time['Month Name']]: record.Statistics
-    //     };
-    //   }
-    // } else {
-    //   airlineStats[record.Time.Year] = {
-    //     [record.Airport.Code]: {
-    //       [record.Time['Month Name']]: record.Statistics
-    //     }
-    //   };
-    // }
-
   }
-  
-  console.log(airlineFilterOptions);
   return [airlineStats, airlineFilterOptions];
 }
 
